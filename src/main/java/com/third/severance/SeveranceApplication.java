1d95086be1acdc4443846f7e4dabcdde4cb87aad
DH K <caskerhello@naver.com>
2024-11-26 23:49:14 +0900

    first commit

---
+++ src/main/java/com/third/severance/SeveranceApplication.java
@@ -0,0 +1,13 @@
+package com.third.severance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeveranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeveranceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/third/severance/SeveranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.third.severance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeveranceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
