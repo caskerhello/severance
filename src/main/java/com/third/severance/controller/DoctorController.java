227fe094ae548fa373102250ca26ce7cd334a20f
caskerhello <caskerhello@naver.com>
2024-12-04 18:40:31 +0900

    Update DoctorController.java

--- src/main/java/com/third/severance/controller/DoctorController.java
+++ src/main/java/com/third/severance/controller/DoctorController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class DoctorController {
 
