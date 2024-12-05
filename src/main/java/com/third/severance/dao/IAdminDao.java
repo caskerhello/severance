90956f5120634e61cf9821c3bf12ed0dcb07a664
jongwook <acornkjw@gamil.com>
2024-12-05 14:49:23 +0900

    test

--- src/main/java/com/third/severance/dao/IAdminDao.java
+++ src/main/java/com/third/severance/dao/IAdminDao.java
@@ -1,10 +1,17 @@
 package com.third.severance.dao;
 
 import com.third.severance.dto.AdminVO;
+import com.third.severance.dto.MemberVO;
+import com.third.severance.dto.Paging;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface IAdminDao {
     AdminVO getAdmin(String adminid);
 
+    int getAllCount(String product, String name, String key);
+
+    List<MemberVO> getMemberList(Paging paging, String key);
 }
